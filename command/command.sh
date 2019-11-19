9dfa28bda96ef1924b4748cdb4f20ba72d9e6395
onion <dkonion@foxmail.com>
2019-11-19 08:46:01 +0800

    nginx 配置文件和常用命令添加

--- command/command.sh
+++ command/command.sh
@@ -9,4 +9,23 @@ function command(){
 	
 	#查看常用端口
 	netstat -nlpt
+	
+	#nginx 命令
+	nginx -t  #检查配置文件正确性
+	nginx -s reload #重载配置文件
+	nginx -s quit  #待nginx进程处理任务完毕后停止
+	nginx -s stop #停止
+	nginx   #启动 
+}
+
+function centos_command(){
+	#查看防火墙状态
+	firewall-cmd --state
+
+	#停止firewall
+	systemctl stop firewalld.service
+
+	#禁止firewall开机启动
+	systemctl disable firewalld.service
+
 }
